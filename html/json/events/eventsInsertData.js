096081aff5d8b9373e60d1936d49e8a1a5ea0d6f
cjii2 <ahmed12345m3@gmail.com>
2025-12-28 17:18:02 +0300

    up

--- html/json/events/eventsInsertData.js
+++ html/json/events/eventsInsertData.js
@@ -1,52 +1,57 @@
-// عدم مساس هاذا سكربت نهائي اذا لم تتوفر خبره كافيه
-
 class Galleries {
     constructor(Father, jsonPath) {
         this.Father = document.getElementById(Father);
         this.jsonPath = jsonPath;
-    };
-
+    }
 
     async loadElements() {
-        const res = await fetch(this.jsonPath);
+        const res = await fetch(this.jsonPath + "?nocache=" + Date.now());
         const data = await res.json();
 
         Object.entries(data.groups).forEach(([groupName, items]) => {
-            this.groupName = groupName;
-            this.items = items;
-            this.Father.insertAdjacentHTML('beforeend', `
-                    <section class="gallry-section ${items[0].direction}">
-                        <div class="group left">
-                        
-                        </div>
-                        
-                        <div aria-hidden class="group left">
+            // console.log(groupName)
+            // إنشاء السيكشن
+            const section = document.createElement("section");
+            section.className = `gallry-section ${items[0].direction}`;
 
-                        </div>
-                    </section>
-                    `);
-                this.imgContainer = document.querySelectorAll('.group');
+            const group1 = document.createElement("div");
+            group1.className = "group left";
+
+            const group2 = document.createElement("div");
+            group2.className = "group left";
+            group2.setAttribute("aria-hidden", "true");
+
+            section.append(group1, group2);
+            this.Father.appendChild(section);
+
+            // إضافة الصور
+            items.forEach(item => {
+                const figure = document.createElement("figure");
+                figure.className = "img-container";
+
+                const img = document.createElement("img");
+                img.src = item.img;
+                img.alt = "";
+
+                figure.appendChild(img);
+
+                group1.appendChild(figure.cloneNode(true));
+                group2.appendChild(figure);
             });
 
+        });
+    }
 
-            for(let index=0; index<this.items.length;index++)
-            this.imgContainer.forEach((e)=>{
-                e.insertAdjacentHTML('beforeend',`
-                    <figure class="img-container">
-                        <img src="${this.items[index]["img"]}" alt="">
-                    </figure>
-                    `);
-            });
-    };
-    activeApp(){
-       this.loadElements();
-    };
-}; 
-   
+    activeApp() {
+        document.addEventListener("DOMContentLoaded", () => {
+            this.loadElements();
+        });
+    }
+}
+
 const activeGalleries = new Galleries(
-    'gallryContainer',
-    '../html/json/events/eventsData.json'
+    "gallryContainer",
+    "../html/json/events/eventsData.json"
 );
 
 activeGalleries.activeApp();
-
